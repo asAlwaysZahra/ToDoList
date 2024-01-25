cdb5f2427534d55d377bd69a5a384a057a87cf6f
Zahra Masoumi <zahraamasoumi@gmail.com>
2024-01-26 00:56:32 +0330

    refactor(models): rename the ToDoList class

---
+++ ToDoList/ToDoList/Models/TheList.cs
@@ -0,0 +1,34 @@
+﻿namespace ToDoList.Models
+{
+    internal class TheList
+    {
+        protected static int _counter = 0;
+        public int Id { get; set; }
+        public List<Task> Tasks { get; set; }
+        public List<Task> Done { get; set; }
+
+        public TheList()
+        {
+            Id = _counter++;
+            Tasks = new List<Task>();
+            Done = new List<Task>();
+        }
+
+        public void AddTask(Task task) => Tasks.Add(task);
+
+        public void RemoveTask(Task task) => Tasks.Remove(task);
+
+        public void DoTask(Task task)
+        {
+            Tasks.Remove(task);
+            Done.Add(task);
+        }
+
+        public List<Task> SortTasks()
+        {
+            List<Task> sorted = new List<Task>(Tasks);
+            sorted.Sort();
+            return sorted;
+        }
+    }
+}
