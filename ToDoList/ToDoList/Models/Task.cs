69a5b64f7ca8373f7649b31bca2f74b455f6e1c5
Zahra Masoumi <zahraamasoumi@gmail.com>
2024-01-26 01:28:36 +0330

    feat(models): add to string method

--- ToDoList/ToDoList/Models/Task.cs
+++ ToDoList/ToDoList/Models/Task.cs
@@ -4,7 +4,7 @@ namespace ToDoList.Models
 {
     public class Task : IComparable<Task>
     {
-        protected static int _counter = 0;
+        protected static int _counter = 1;
         public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
@@ -31,6 +31,11 @@ namespace ToDoList.Models
                 return Deadline.CompareTo(other.Deadline);
 
             return other.Priority.CompareTo(Priority);
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} | {Priority} | {Title} | Due: {Deadline}\n  - {Description}\n    Created at: {CreationDate}";
         }
     }
 }
