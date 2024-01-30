e1abbc27601acb53753b8f1a37aabec76d3a564e
Zahra Masoumi <zahraamasoumi@gmail.com>
2024-01-30 17:28:59 +0330

    refactor(Main): refactor Program.cs now we have a cleaner code with seperated methods for each part

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -4,51 +4,10 @@ using Task = ToDoList.Models.Task;
 using System.Text.Json;
 
 // test main functionality ----------------------------------
-
-Task task1 = new("AA task", "a task with same deadline and priority as task 2 and different creation date", DateTime.Now.AddDays(2), Priority.LowPriority);
-Task task2 = new("A task", "another task with same deadline as task 3 and less priority", DateTime.Now.AddDays(2), Priority.LowPriority);
-Task task3 = new("Clean your laptop", "its about 1 month you have not cleaned your laptop!", DateTime.Now.AddDays(2), Priority.HighPriority);
-Task task4 = new("Go to the dentist", "your theeth need to be checked", DateTime.Now.AddDays(7), Priority.LowPriority);
-Task task5 = new("DB homework", "the final project, phase 2", DateTime.Now.AddDays(3), Priority.Neutral);
-Task task6 = new("Reconcile eith your friend", "you know the right thing to do", DateTime.Now.AddDays(1), Priority.Critical);
-task1.CreationDate = DateTime.Now.AddDays(-1);
-
-TheList myList = new();
-myList.AddTask(task1);
-myList.AddTask(task2);
-myList.AddTask(task3);
-myList.AddTask(task4);
-myList.AddTask(task5);
-myList.AddTask(task6);
-
-Console.WriteLine(myList.ToString());
-
-Console.WriteLine("After Sorting:\n");
-
-Console.WriteLine(myList.GetSortedString());
-
-myList.DoTask(task2);
-myList.RemoveTask(task3);
+Test();
 
 // test reading from file ----------------------------------
-
-List<Task> tasks = ReadFile("./tasksss.json");
-
-// do all tasks
-foreach (Task task in tasks)
-    myList.DoTask(task);
-
-// add new tasks (read from file)
-foreach (Task task in tasks)
-    myList.AddTask(task);
-
-// comment out these lines
-// and comment previos prints
-// to see the result.
-
-//Console.WriteLine(myList.ToString());
-//Console.WriteLine("After Sorting:\n");
-//Console.WriteLine(myList.GetSortedString());
+TestReadingFile("./tasksss.json");
 
 static List<Task> ReadFile(string path)
 {
@@ -56,10 +15,70 @@ static List<Task> ReadFile(string path)
     {
         string json = File.ReadAllText(path);
         var tasks = JsonSerializer.Deserialize<List<Task>>(json);
+
+        if (tasks == null)
+            throw new ArgumentException("The path provided does not contain any data!");
+
         return tasks;
     }
     else
     {
         throw new Exception("file not found!");
     }
+}
+
+static TheList SampleTasks()
+{
+    Task task1 = new("AA task", "a task with same deadline and priority as task 2 and different creation date", DateTime.Now.AddDays(2), Priority.LowPriority);
+    Task task2 = new("A task", "another task with same deadline as task 3 and less priority", DateTime.Now.AddDays(2), Priority.LowPriority);
+    Task task3 = new("Clean your laptop", "its about 1 month you have not cleaned your laptop!", DateTime.Now.AddDays(2), Priority.HighPriority);
+    Task task4 = new("Go to the dentist", "your theeth need to be checked", DateTime.Now.AddDays(7), Priority.LowPriority);
+    Task task5 = new("DB homework", "the final project, phase 2", DateTime.Now.AddDays(3), Priority.Neutral);
+    Task task6 = new("Reconcile eith your friend", "you know the right thing to do", DateTime.Now.AddDays(1), Priority.Critical);
+    task1.CreationDate = DateTime.Now.AddDays(-1);
+
+    TheList myList = new();
+    myList.AddTask(task1);
+    myList.AddTask(task2);
+    myList.AddTask(task3);
+    myList.AddTask(task4);
+    myList.AddTask(task5);
+    myList.AddTask(task6);
+
+    return myList;
+}
+
+static void TestReadingFile(string path)
+{
+    List<Task> tasks = ReadFile(path);
+    TheList myList = new();
+
+    // add new tasks (read from file)
+    foreach (Task task in tasks)
+        myList.AddTask(task);
+
+    Console.WriteLine(myList.ToString());
+
+    Console.WriteLine("After Sorting:\n");
+
+    Console.WriteLine(myList.GetSortedString());
+
+    // do all tasks :|
+    foreach (Task task in tasks)
+        myList.DoTask(task);
+}
+
+static void Test()
+{
+    TheList myList = SampleTasks();
+
+    Console.WriteLine(myList.ToString());
+
+    Console.WriteLine("After Sorting:\n");
+
+    Console.WriteLine(myList.GetSortedString());
+
+    // test methods
+    myList.DoTask(myList.Tasks[1]);
+    myList.RemoveTask(myList.Tasks[2]);
 }
