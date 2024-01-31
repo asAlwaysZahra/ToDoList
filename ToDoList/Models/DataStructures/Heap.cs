b1bacb786602e40036583dc6d385d60958916e9a
Zahra Masoumi <zahraamasoumi@gmail.com>
2024-02-01 02:44:59 +0330

    feat(data structures): change heap class to abstract implement and inhertance two other classes: min heap and map heap

--- ToDoList/Models/DataStructures/Heap.cs
+++ ToDoList/Models/DataStructures/Heap.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class BinaryHeap<T> where T : IComparable<T>
+namespace ToDoList.Models.DataStructures;
+
+public abstract class BinaryHeap<T> where T : IComparable<T>
 {
-    private IList<T> _heap;
+    protected IList<T> _heap;
 
     public BinaryHeap(T[]? elements = null)
     {
@@ -57,44 +59,7 @@ public class BinaryHeap<T> where T : IComparable<T>
         HeapifyUp(Count - 1);
     }
 
-    private void HeapifyDown(int i)
-    {
-        var leftChild = (i * 2) + 1;
-        var rightChild = (i * 2) + 2;
-        var biggest = i;
+    protected abstract void HeapifyDown(int i);
 
-        if (leftChild < Count && _heap[leftChild].CompareTo(_heap[biggest]) > 0)
-        {
-            biggest = leftChild;
-        }
-
-        if (rightChild < Count && _heap[rightChild].CompareTo(_heap[biggest]) > 0)
-        {
-            biggest = rightChild;
-        }
-
-        if (biggest != i)
-        {
-            // swap them
-            (_heap[biggest], _heap[i]) = (_heap[i], _heap[biggest]);
-
-            HeapifyDown(biggest);
-        }
-    }
-
-    private void HeapifyUp(int i)
-    {
-        // first non-leaf node
-        var parent = (i - 1) / 2;
-
-        while (i > 0 && _heap[i].CompareTo(_heap[parent]) > 0)
-        {
-            // swap
-            (_heap[i], _heap[parent]) = (_heap[parent], _heap[i]);
-
-            i = parent;
-
-            parent = (i - 1) / 2;
-        }
-    }
+    protected abstract void HeapifyUp(int i);
 }
---
+++ ToDoList/Models/DataStructures/MaxHeap.cs
@@ -0,0 +1,45 @@
+﻿namespace ToDoList.Models.DataStructures;
+
+public class MaxHeap<T> : BinaryHeap<T> where T : IComparable<T>
+{
+    protected override void HeapifyDown(int i)
+    {
+        var leftChild = (i * 2) + 1;
+        var rightChild = (i * 2) + 2;
+        var biggest = i;
+
+        if (leftChild < Count && _heap[leftChild].CompareTo(_heap[biggest]) < 0)
+        {
+            biggest = leftChild;
+        }
+
+        if (rightChild < Count && _heap[rightChild].CompareTo(_heap[biggest]) < 0)
+        {
+            biggest = rightChild;
+        }
+
+        if (biggest != i)
+        {
+            // swap them
+            (_heap[biggest], _heap[i]) = (_heap[i], _heap[biggest]);
+
+            HeapifyDown(biggest);
+        }
+    }
+
+    protected override void HeapifyUp(int i)
+    {
+        // first non-leaf node
+        var parent = (i - 1) / 2;
+
+        while (i > 0 && _heap[i].CompareTo(_heap[parent]) < 0)
+        {
+            // swap
+            (_heap[i], _heap[parent]) = (_heap[parent], _heap[i]);
+
+            i = parent;
+
+            parent = (i - 1) / 2;
+        }
+    }
+}
